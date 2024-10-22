558c87719b76e17f06b7208138664bd2e241f568
khaledsaifulla010 <khaledsaifulla010@gmail.com>
2024-10-22 10:13:03 +0600

    project enviroment setup and add google font

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Blogs from './Components/1.Blogs/Blogs'
+
+function App() {
+  
+
+  return (
+    <>
+
+    <Blogs></Blogs>
+      
+    </>
+  )
+}
+
+export default App
